efda5dc6e13f78ecf39e6b1792fbe1aed026594c
Grace Mora <50357533+gizmora@users.noreply.github.com>
2024-11-18 23:21:56 +0800

    chore: connect chrome ext to node.js server

--- background.js
+++ background.js
@@ -1,1 +1,29 @@
 console.log('This is your service worker that runs in background.');
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  console.log('This chrome ext powered by Vertex AI');
+
+  if (request.action === 'generatePrompt') {
+    let url = 'http://localhost:8888/vertex-ai/generate-prompt';
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    fetch(url, options)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log('Vertex AI response: ')
+      console.log(data);
+
+      sendResponse({data: data.promptResponse})
+    }).catch((error) => {
+      console.error(error);
+      sendResponse({error: error});
+    });
+
+    return true;
+  }
+});
--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,18 @@
-console.log('This is a popup');
+console.log('POPUP JS');
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('submit-prompt').addEventListener('click', () => {
+    console.log('CLICKED')
+  
+    chrome.runtime.sendMessage({ action: 'generatePrompt'}, (response) => {
+      if (response.error) {
+        console.error('Error:', response.error);
+      } else {
+        const generatedText = response.generatedText;
+        // Do something with the generated text
+        
+        document.getElementById('prompt-response').innerText(generatedText);
+      }
+    });
+  });
+});
