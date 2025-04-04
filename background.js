8b8a6adc57f3e71f6db54e3f2f27160cbbeff00a
Grace Mora <50357533+gizmora@users.noreply.github.com>
2025-04-04 22:12:19 +0800

    chore: add inside IIFE for background;

--- background.js
+++ background.js
@@ -1,9 +1,27 @@
-console.log('This is your service worker that runs in background.');
+(() => {
+  console.log('This is your service worker that runs in background.');
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  console.log(`From ${sender.tab}: ${sender.url}`);
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    console.log(`From ${sender.tab}: ${sender.url}`);
 
-  if (request.action === 'generatePrompt') {
+    if (request.action === 'generatePrompt' && request.prompt) {
+      sendPromptToVertexAI(request.prompt, sendResponse);
+      return true;
+    }
+  });
+
+  let sidebarVisible = false; 
+
+  chrome.action.onClicked.addListener((tab) => {
+    sidebarVisible = !sidebarVisible; // Toggle visibility
+
+    chrome.tabs.sendMessage(tab.id, { 
+      action: 'toggleSidebar', 
+      visible: sidebarVisible 
+    });
+  });
+
+  const sendPromptToVertexAI = function (prompt, cb) {
     let url = 'https://vertex-demo-service-851787392919.us-central1.run.app/vertex-ai/generate-prompt';
     const options = {
       method: 'POST',
@@ -12,75 +30,40 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       }
     }
 
+    if (prompt && prompt !== '') {
+      options.body = JSON.stringify({
+        prompt: {
+          text: prompt
+        }
+      });
+    }
+
     fetch(url, options)
     .then((res) => res.json())
     .then((data) => {
       console.log('Vertex AI response: ')
       console.log(data);
 
-      sendResponse({data: data.promptResponse})
+      cb({data: data.promptResponse})
     }).catch((error) => {
       console.error(error);
-      sendResponse({error: error});
+      cb({error: error});
     });
+  };
 
-    return true;
-  }
-});
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    console.log('%c SHERLOCK AI: HELLO CLICK CASE LOG', 'background: #222; color: yellow;');
+    console.log(tabs);
+    chrome.tabs.sendMessage(tabs[0].id, { action: 'clickCaseLog' }, function (response) {
 
-let sidebarVisible = false; 
-
-chrome.action.onClicked.addListener((tab) => {
-  sidebarVisible = !sidebarVisible; // Toggle visibility
-
-  chrome.tabs.sendMessage(tab.id, { 
-    action: 'toggleSidebar', 
-    visible: sidebarVisible 
-  });
-});
-
-const sendPromptToVertexAI = function (prompt, cb) {
-  let url = 'https://vertex-demo-service-851787392919.us-central1.run.app/vertex-ai/generate-prompt';
-  const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  }
-
-  if (prompt && prompt !== '') {
-    options.body = JSON.stringify({
-      prompt: {
-        text: prompt
+      if (response) {
+        if (response.success) {
+          console.log('Button clicked successfully!');
+        } else {
+          console.error('Failed to click button:', response.error);
+        }
       }
+
     });
-  }
-
-  fetch(url, options)
-  .then((res) => res.json())
-  .then((data) => {
-    console.log('Vertex AI response: ')
-    console.log(data);
-
-    cb({data: data.promptResponse})
-  }).catch((error) => {
-    console.error(error);
-    cb({error: error});
   });
-};
-
-chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-  console.log('%c HELLO CLICK CASE LOG', 'background: #222; color: yellow;');
-  console.log(tabs);
-  chrome.tabs.sendMessage(tabs[0].id, { action: 'clickCaseLog' }, function (response) {
-
-    if (response) {
-      if (response.success) {
-        console.log('Button clicked successfully!');
-      } else {
-        console.error('Failed to click button:', response.error);
-      }
-    }
-
-  });
-});
+})();
