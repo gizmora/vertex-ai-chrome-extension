86b800582e4ac85e7b32c4c1b9f6e600f6275359
Grace Mora <50357533+gizmora@users.noreply.github.com>
2025-04-26 23:33:48 +0800

    chore: initial content.js

--- scripts/content.js
+++ scripts/content.js
@@ -2,5 +2,16 @@ import CASE_SCRAPER from "../src/case-scraper";
 import SHADOW_DOM from "../src/shadow-dom";
 
 (() => {
+  const CASE_STATE = {
+    caseId: '',
+    contactUsForm: [],
+    timeline: [],
+    isThreadExpanded: false,
+  }
+
+
+  // initialize SHADOW DOM
+  SHADOW_DOM.init();
+
   
 })();
