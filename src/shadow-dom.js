ee1568d47232622f8f88bd829bf9c6b6b14834e7
Grace Mora <50357533+gizmora@users.noreply.github.com>
2025-04-26 23:35:12 +0800

    chore: initial shadow dom

--- src/shadow-dom.js
+++ src/shadow-dom.js
@@ -1,126 +1,88 @@
+import UTILS from "./utils";
+
 const SHADOW_DOM = {
-  containerId: 'sherlock-ai',
+  id: 'sherlock-ai',
   _shadowRoot: null,
-  mainContainerSelector: '.main-container',
-  homeTemplate: 
-  `<div class="case-input">
-    <div class="main-heading">Case Details</div>
-    <div class="subheading">Narrate case details here to identify points for correction</div>
-    <textarea placeholder="Ex: Email exchange between agent and customer" id="case-details"></textarea>
-    <div id="error-label"></div>
-  </div>
-  <div class="__action-btn">
-    <button id="submit-prompt" class="mat-raised-button">Run Diagnostics</button>
-    <button id="case-logs" class="mat-raised-button">Click Case Logs</button>
-    <div id="spinner"></div>
-  </div>`,
-  resultsTemplate: 
-  `<div id="prompt-response">
-    <div id="rules-table"></div>
-  </div>`,
+  pageTemplate: {
+    home: `<div class="case-input">
+      <div class="main-heading">Case Details</div>
+      <div class="subheading">Narrate case details here to identify points for correction</div>
+      <textarea placeholder="Ex: Email exchange between agent and customer" id="case-details"></textarea>
+      <div id="error-label"></div>
+    </div>
+    <div class="__action-btn">
+      <button id="submit-prompt" class="mat-raised-button">Run Diagnostics</button>
+      <button id="case-logs" class="mat-raised-button">Click Case Logs</button>
+      <div id="spinner"></div>
+    </div>`,
+    results: `<div id="prompt-response">
+      <div id="rules-table"></div>
+    </div>`,
+  },
+
+  createDiv: function (id = '') {
+    let div = document.createElement('div');
+
+    if (id !== '') {
+      div.id = id;
+    }
+
+    return div;
+  },
 
   init: function () {
-    let mainDiv = document.createElement('div');
-    mainDiv.id = this.containerId;
-    document.body.appendChild(mainDiv);
-    this._shadowRoot = mainDiv.attachShadow({ mode: 'open' });
-    this.addMessageListener();
+    let sherlock = this.createDiv('sherlock-ai');
+    document.body.appendChild(sherlock);
+
+    this._shadowRoot = sherlock.attachShadow({ mode: 'open' });
+    this.setMessageListener();
+    // route change listner
   },
 
-  addChild: function (el) {
-    let _self = this;
-    
-    if (_self._shadowRoot) {
-      _self._shadowRoot.appendChild(el);
-    } else {
-      console.error("Shadow root not initialized");
-    }
-  },
-
-  addMessageListener: function() {
+  setMessageListener: function () {
     let _self = this;
     chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       if (request.action === 'toggleSidebar') {
-        _self.setLayout();
+        _self.setSidebar();
       }
     });
   },
 
-  sendMessage: function(msg, cb) {
-    chrome.runtime.sendMessage(msg, (response) => {
-      cb(response);
-    });
-  },
-
-  fetchUrl: function (url, cb) {
-    fetch(chrome.runtime.getURL(url))
-      .then((response) => response.text())
-      .then(res => {
-        cb(res);
-      }).catch(error => {
-        console.error(error)
-      });
-  },
-
-  setLayout: function() {
+  setSidebar: async function () {
     let _self = this;
-
     const sidebar = _self._shadowRoot.getElementById('chr-sidebar');
 
     if (sidebar) {
       sidebar.style.display = sidebar.style.display === 'none' ? 'block' : 'none';
     } else {
-      const newSidebar = document.createElement('div');
-      newSidebar.id = 'chr-sidebar';
+      const newSidebar = _self.createDiv('chr-sidebar');
+      const page = await UTILS.fetchUrl('../popup/landing.html');
 
-      _self.fetchUrl('../popup/landing.html', (page) => {
-        newSidebar.innerHTML = page;
-        
-        _self.injectFont('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
-        _self.setStyling();
-        _self._shadowRoot.appendChild(newSidebar);
-        _self.addChild(newSidebar);
-        _self.setHeaderIcon();
-        _self.addNavListener();
-        _self.loadContent('home');
-        _self.addSubmitListener();
-        // _self.caseLogBtnListener();
-      });
+      newSidebar.innerHTML = page;
+
+      UTILS.injectFont();
+      _self.setStyling();
+      _self.addChild(newSidebar);
+      _self.setHeaderIcon();
+      _self.setNavListener();
+      _self.loadContent('home');
+      _self.setSubmitListener();
     }
   },
 
-  injectFont: function(url) {
-    const preconnect1 = document.createElement('link');
-    preconnect1.rel = 'preconnect';
-    preconnect1.href = 'https://fonts.googleapis.com';
-    document.head.appendChild(preconnect1);
-
-    const preconnect2 = document.createElement('link');
-    preconnect2.rel = 'preconnect';
-    preconnect2.href = 'https://fonts.gstatic.com';
-    preconnect2.crossOrigin = 'anonymous';
-    document.head.appendChild(preconnect2);
-
-    const link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = url;
-    document.head.appendChild(link);
-  },
-
-  setStyling: function() {
+  setStyling: async function() {
     let _self = this;
 
-    _self.fetchUrl('../popup/styles.css', (css) => {
-      const styledEl = document.createElement('style');
-      styledEl.textContent = css;
+    const css = await UTILS.fetchUrl('../popup/styles.css');
+    const styledEl = document.createElement('style');
+    styledEl.textContent = css;
 
-      _self.addChild(styledEl);
-    });
+    _self.addChild(styleEl);
   },
 
-  setHeaderIcon: function () {
+  setHeaderIcon: async function () {
     let _self = this;
-    let iconPath = chrome.runtime.getURL('../assets/images/icon-128.png');
+    let iconPath = await fetchUrl('../assets/images/icon-128.png');
     let iconContainer = _self._shadowRoot.getElementById('sherlock-icon');
 
     if (iconContainer) {
@@ -130,14 +92,14 @@ const SHADOW_DOM = {
     }
   },
 
-  addNavListener: function () {
+  setNavListener: function () {
     let _self = this;
     const nav = _self._shadowRoot.querySelector('.nav');
 
     nav.addEventListener('click', (event) => {
       const buttonId = event.target.dataset.navBtnName;
-    
-      if (event.target.classList.contains('nav-btn')) { 
+
+      if (event.target.classList.contains('nav-btn')) {
         _self.loadContent(buttonId);
       }
     });
@@ -153,62 +115,88 @@ const SHADOW_DOM = {
     });
 
     let contentSection = mainContainer.querySelector(`#${page}-content`);
+
     if (!contentSection) {
-      contentSection = document.createElement('div');
-      contentSection.id = `${page}-content`;
+      contentSection = _self.createDiv(`${page}-content`);
       contentSection.classList.add('section-content');
       mainContainer.appendChild(contentSection);
-
-      switch (page) {
-        case 'home':
-          contentSection.innerHTML = _self.homeTemplate;
-          break;
-        case 'results':
-          contentSection.innerHTML = _self.resultsTemplate;
-          break;
-        default:
-          contentSection.innerHTML = _self.homeTemplate;
-      }
+      contentSection.innerHTML = _self.pageTemplate[page] || _self.pageTemplate.home;
     }
 
     contentSection.style.display = 'block';
   },
 
-  addSubmitListener() {
+  setSubmitListener: function() {
     let _self = this;
     const submitButton = _self._shadowRoot.getElementById('submit-prompt');
 
     if (submitButton) {
-      submitButton.addEventListener('click', () => {
+      submitButton.addEventListener('click', async () => {
         const caseDetails = _self._shadowRoot.getElementById('case-details').value;
+        const isValidInput = UTILS.preprocessPrompt(caseDetails);
 
-        if (_self.preprocessInput(caseDetails)) {
-          _self._shadowRoot.getElementById('error-label').textContent = '';
+        if (isValidInput) {
+          _self.setErrorMsg('');
           _self.toggleLoader(true);
 
-          _self.sendMessage({action: 'generatePrompt', prompt: caseDetails}, (response) => {
-            _self.toggleLoader(false);
-            if (response.error) {
-              console.error(response.error);
-            } else {
-              _self.loadContent('results');
-              _self._shadowRoot.getElementById('prompt-response').style.display = 'block';
+          const results = await UTILS.sendMessage({action: 'generatePrompt', prompt: caseDetails});
+          _self.toggleLoader(false);
 
-              const generatedText = response.data;
-              const tableContainer = _self._shadowRoot.getElementById('rules-table');
-              const myTable = _self.createResultsTable(generatedText.rules);
-              
-              tableContainer.innerHTML = '';
-              tableContainer.appendChild(myTable); 
-            }
-          });
+          if (response.error) {
+            console.error(response.error);
+            return;
+          }
+
+          _self.loadContent('results');
+          const promptResponse = _self._shadowRoot.getElementById('prompt-response');
+
+          if (promptResponse) {
+            promptResponse.style.display = 'block';
+          }
+          
+          const tableContainer = _self._shadowRoot.getElementById('rules-table');
+
+          if (tableContainer && response.data) {
+            const generatedText = response.data;
+            const myTable = _self.createResultsTable(generatedText.rules);
+            tableContainer.innerHTML = '';
+            tableContainer.appendChild(myTable); 
+          }
+
+        } else {
+          _self.setErrorMsg('Insufficient content for checking.');
         }
-      })
+      });
     } else {
       console.error("Submit button not found in Shadow DOM");
     }
+  },
+
+  setErrorMsg: function (msg) {
+    let errorLabel = this._shadowRoot.getElementById('error-label');
+    errorLabel.textContent = msg;
+  },
+
+  addChild: function (el) {
+    let _self = this;
     
+    if (_self._shadowRoot) {
+      _self._shadowRoot.appendChild(el);
+    } else {
+      console.error("Shadow root not initialized");
+    }
+  },
+
+  toggleLoader: function(show) {
+    let _self = this;
+
+    const loader = _self._shadowRoot.getElementById('spinner');
+
+    if (loader) {
+      loader.style.display = show? 'inline-block' : 'none';
+    }
   }
-};
+
+}
 
 export default SHADOW_DOM;
